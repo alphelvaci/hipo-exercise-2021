c411621232630b82703f27b15802ad157d8a02df
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2021-11-04 15:21:12 +0300

    Relate employees to companies using foreignkey instead of manytomanyfield
    A foreignkey relation makes more sense as there can be companies without employees but there cannot be employees without companies.

--- companies/models.py
+++ companies/models.py
@@ -3,23 +3,23 @@ from django.db import models
 # Create your models here.
 
 
-class Employee(models.Model):
-    name = models.CharField(max_length=25)
-    surname = models.CharField(max_length=25)
-
-    def __str__(self):
-        return f"{self.name} {self.surname}"
-
-
 class Company(models.Model):
     name = models.CharField(max_length=45)
-    employees = models.ManyToManyField(Employee)
 
     class Meta:
         verbose_name_plural = "companies"
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=25)
+    surname = models.CharField(max_length=25)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
 
 
 class Card(models.Model):
