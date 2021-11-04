bf186a92128b1f674e5cf8cb14a2a419d4e36156
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2021-11-04 14:42:38 +0300

    Create the employee model

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=25)
+    surname = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
