436ff361a820ab048c0b7d893b6be42eb6647997
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2021-11-04 15:47:51 +0300

    Add different contract types for employees and cards

--- companies/models.py
+++ companies/models.py
@@ -14,8 +14,14 @@ class Company(models.Model):
 
 
 class Employee(models.Model):
+    CONTRACT_TYPE_CHOICES = [
+        ('small_town', 'Small Town'),
+        ('city_center', 'City Center')
+    ]
+
     name = models.CharField(max_length=25)
     surname = models.CharField(max_length=25)
+    contract_type = models.CharField(max_length=15, choices=CONTRACT_TYPE_CHOICES)
     company = models.ForeignKey(Company, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
@@ -24,6 +30,10 @@ class Employee(models.Model):
 
 class Card(models.Model):
     employee = models.OneToOneField(Employee, on_delete=models.PROTECT)
+
+    @property
+    def card_type(self):
+        return self.employee.contract_type
 
     def __str__(self):
         return f"{self.employee}'s Card (Card {self.id})"
