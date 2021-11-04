9cb3f318694373da8a96749035b2a511d0200c66
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2021-11-04 16:29:44 +0300

    Create the restaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return f"{self.name}"
