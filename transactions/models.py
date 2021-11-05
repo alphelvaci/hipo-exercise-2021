47f742b542928ef53393c10c595a7dbffc3a1255
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2021-11-05 11:44:25 +0300

    Create a base transaction model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Transaction(models.Model):  # any transaction
+    amount = models.DecimalField(max_digits=11, decimal_places=2)  # up to 999 millon # TO-DO dont allow zero value
+    date = models.DateTimeField()
+
+    def __str__(self):
+        if self.amount < 0:
+            amount_string = str(self.amount)
+        else:
+            amount_string = "+" + str(self.amount)
+        return f"Transaction {self.id} ({amount_string})"
