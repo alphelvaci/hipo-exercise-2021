3805790cde78b92dcc7ca27480ffce74656d2150
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2021-11-05 12:00:24 +0300

    Create company card and restaurant transaction models

--- transactions/models.py
+++ transactions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from companies.models import Company, Card
+from restaurants.models import Restaurant
 
 # Create your models here.
 
@@ -13,3 +15,27 @@ class Transaction(models.Model):  # any transaction
         else:
             amount_string = "+" + str(self.amount)
         return f"Transaction {self.id} ({amount_string})"
+
+
+class CompanyTransaction(models.Model):  # any company transaction
+    transaction = models.OneToOneField(Transaction, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.company} Transaction {self.id}"
+
+
+class CardTransaction(models.Model):  # any card transaction
+    transaction = models.OneToOneField(Transaction, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.card} Transaction {self.id}"
+
+
+class RestaurantTransaction(models.Model):  # any restaurant transaction
+    transaction = models.OneToOneField(Transaction, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.restaurant} Transaction {self.id}"
