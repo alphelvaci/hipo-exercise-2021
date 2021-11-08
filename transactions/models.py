108c8eb3219886a6883a8d8823cc4cc0319ee0e8
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2021-11-08 16:12:27 +0300

    Create the CompanyFundingTransaction model

--- transactions/models.py
+++ transactions/models.py
@@ -5,3 +5,15 @@ from restaurants.models import Restaurant
 # Create your models here.
 
 
+class CompanyFundingTransaction(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=11, decimal_places=2)  # up to 999 millon
+    date = models.DateTimeField()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(amount__gt=0), name='amount_gt_0'),
+        ]
+
+    def __str__(self):
+        return f"Funding {self.id} ({self.company} +{self.amount})"
