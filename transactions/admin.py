02751e91271c145a82244521153be187d4ff3f33
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2021-11-11 02:20:14 +0300

    Expose models to django admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CompanyFundingTransaction, CompanyCardTransaction, CardRestaurantTransaction
 
 # Register your models here.
+
+admin.site.register([CompanyFundingTransaction, CompanyCardTransaction, CardRestaurantTransaction])
